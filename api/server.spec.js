9332f8be87510f722696e1676f892f5fce090200
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-18 08:19:44 -0700

    building first test

---
+++ api/server.spec.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const server = require("./server");
+
+describe("server.js", () => {
+  describe("GET /", () => {
+    it("returns 200 OK", () => {
+      return request(server)
+        .get("/")
+        .then(res => {
+          expect(res.status.toBe(200));
+        });
+    });
+  });
+});
