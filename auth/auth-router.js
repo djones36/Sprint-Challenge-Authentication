c6f8698c5b3a7210576b1946cbd078353cf48c6c
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-18 08:40:21 -0700

    built get endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Users = require("./auth-model");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+});
+
+router.get("/users", (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json({ loggedInUser: req.username, users });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "failed to display users", error });
+    });
 });
 
 module.exports = router;
