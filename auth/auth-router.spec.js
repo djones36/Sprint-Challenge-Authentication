62feda658a746e1059d363d5078bd09de6a46ee0
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-18 10:02:37 -0700

    building test on endpoints finished two test on the register

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,3 +1,27 @@
 const request = require("supertest");
 const server = require("../api/server");
 const db = require("../database/dbConfig");
+
+describe("auth-router.js", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+
+  //register end point
+  describe("POST /api/auth/register", () => {
+    it("returns status 500", () => {
+      return request(server)
+        .post("/api/auth/register")
+        .then(res => expect(res.status).toBe(500));
+    });
+    it("returns status 201", () => {
+      const user = { username: "testUser", password: "passwordTest" };
+      return request(server)
+        .post("/api/auth/register")
+        .send(user)
+        .then(res => {
+          expect(res.status).toBe(201);
+        });
+    });
+  });
+});
